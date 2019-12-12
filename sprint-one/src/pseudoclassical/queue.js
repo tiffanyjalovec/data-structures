9219a2743d863e104431b8ef068b583b17d18057
Tiffany Jalovec <carr.tiffany@gmail.com>
2019-12-12 12:15:25 -0700

    Part 1 complete

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,22 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.left = 0;
+  this.right = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.right++] = value;
+};
 
+Queue.prototype.dequeue = function() {
+  if (this.right > this.left) {
+    let value = this.storage[this.left];
+    delete this.storage[this.left];
+    this.left++;
+    return value;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.right - this.left;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,21 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.top = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.top++] = value;
+};
 
+Stack.prototype.pop = function() {
+  if (this.top > 0) {
+    this.top--;
+    let value = this.storage[this.top];
+    delete this.storage[this.top];
+    return value;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.top;
+};
