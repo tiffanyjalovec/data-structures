c4e78b704a76eed5d319171586de008f347bee9b
Tiffany Jalovec <carr.tiffany@gmail.com>
2019-12-15 09:08:00 -0700

    es6 solo we found out after the sprint that we didn't work this problem

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,27 @@
 class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(storage, left, right) {
+    this.storage = {};
+    this.left = 0;
+    this.right = 0;
+  }
+
+  enqueue(value) {
+    this.storage[this.right++] = value;
+  }
+
+  dequeue() {
+    if (this.right > this.left) {
+      let value = this.storage[this.left];
+      delete this.storage[this.left];
+      this.left++;
+      return value;
+    }
+  }
+
+  size() {
+    return this.right - this.left;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,26 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(storage, top) {
+    this.storage = {};
+    this.top = 0;
   }
 
+
+  push(value) {
+    this.storage[this.top++] = value;
+  }
+
+  pop() {
+    if (this.top > 0) {
+      this.top--;
+      let value = this.storage[this.top];
+      delete this.storage[this.top];
+      return value;
+    }
+  }
+
+  size() {
+    return this.top;
+  }
 }
